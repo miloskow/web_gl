ff0b6e891ba91bab6103c35db58f44f18d3f989b
Milosz <milos.kow@gmail.com>
2024-06-01 13:43:45 +0200

    Szesciokat rysowanie

---
+++ scripszesciokat.js
@@ -0,0 +1,139 @@
+console.error('works');
+
+vertexShaderTxt = `
+precision mediump float;
+
+attribute vec2 vertPosition;
+attribute vec3 vertColor;
+
+varying vec3 fragColor;
+
+void main() {
+    fragColor = vertColor;
+    gl_Position = vec4(vertPosition, 0.0, 1.0);
+}
+`
+const fragmentShaderTxt = `
+precision mediump float;
+
+varying vec3 fragColor;
+
+void main() {
+    gl_FragColor = vec4(fragColor, 1.0);
+}
+`
+
+const Hexagon = function () {
+    const canvas = document.getElementById('main-canvas');
+    const gl = canvas.getContext('webgl');
+    let canvasColor = [0.2, 0.2, 0.2];
+    
+
+    checkGl(gl);
+
+    gl.clearColor(...canvasColor, 1.0);  // R,G,B, A 
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vertexShader, vertexShaderTxt);
+    gl.shaderSource(fragmentShader, fragmentShaderTxt);
+
+    gl.compileShader(vertexShader);
+    gl.compileShader(fragmentShader);
+
+    const program = gl.createProgram();
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+
+    gl.linkProgram(program);
+
+    gl.detachShader(program, vertexShader);
+    gl.detachShader(program, fragmentShader);
+
+    gl.validateProgram(program);
+
+    let hexagonVerts = [
+    //  X,    Y
+        0.0, 0.0,
+        0.0, 1.0,
+        1.73, 0.5,
+        1.73, -0.5,
+        0.0, -1.0,
+        -1.73, -0.5,
+        -1.73, 0.5,
+        0.0, 1.0
+    ]
+
+    let colors = [
+    //  r    g    b
+        1.0, 1.0, 1.0,
+        1.0, 0.0, 0.0,
+        1.0, 1.0, 0.0,
+        1.0, 0.0, 1.0,
+        0.0, 0.0, 1.0,
+        1.0, 0.0, 1.0,
+        1.0, 1.0, 0.0,
+        1.0, 0.0, 0.0
+    ]
+
+    const hexagonVertBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, hexagonVertBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(hexagonVerts), gl.STATIC_DRAW);
+
+
+    const posAttrLoc = gl.getAttribLocation(program, 'vertPosition');
+    gl.vertexAttribPointer(
+        posAttrLoc,
+        2,
+        gl.FLOAT,
+        gl.FALSE,
+        2 * Float32Array.BYTES_PER_ELEMENT,
+        0
+    );
+
+    gl.enableVertexAttribArray(posAttrLoc);
+
+    const hexagonColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, hexagonColorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    const colorAttrLoc = gl.getAttribLocation(program, 'vertColor');
+    gl.vertexAttribPointer(
+        colorAttrLoc,
+        3,
+        gl.FLOAT,
+        gl.FALSE,
+        3 * Float32Array.BYTES_PER_ELEMENT,
+        0
+    );
+
+    gl.enableVertexAttribArray(colorAttrLoc);
+    // render time
+
+    gl.useProgram(program);
+
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, hexagonVerts.length / 2);
+    
+
+    
+
+}
+
+function checkGl(gl) {
+    if (!gl) {console.log('WebGL not supported, use another browser');}
+}
+
+function checkShaderCompile(gl, shader) {
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.error('shader not compiled', gl.getShaderInfoLog(shader));
+    }
+}
+
+function checkLink(gl, program) {
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error('ERROR linking program!', gl.getProgramInfoLog(program));
+    }
+}
