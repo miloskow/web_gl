7bc93419dbe524e70bb6466a531e412022c112b1
Milosz <milos.kow@gmail.com>
2024-06-01 11:34:50 +0200

    Rysowanie kwadratu

---
+++ script.js
@@ -0,0 +1,135 @@
+console.error('works');
+
+vertexShaderTxt = `
+precision mediump float;
+
+attribute vec2 vertPosition;
+attribute vec3 vertColor;
+
+varying vec3 fragColor;
+
+void main() {
+    fragColor = vertColor;
+    gl_Position = vec4(vertPosition, 0.0, 1.0);
+}
+`
+const fragmentShaderTxt = `
+precision mediump float;
+
+varying vec3 fragColor;
+
+void main() {
+    gl_FragColor = vec4(fragColor, 1.0);
+}
+`
+
+const Square = function () {
+    const canvas = document.getElementById('main-canvas');
+    const gl = canvas.getContext('webgl');
+    let canvasColor = [0.2, 0.2, 0.2];
+
+    checkGl(gl);
+
+    gl.clearColor(...canvasColor, 1.0);  // R,G,B, A 
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vertexShader, vertexShaderTxt);
+    gl.shaderSource(fragmentShader, fragmentShaderTxt);
+
+    gl.compileShader(vertexShader);
+    gl.compileShader(fragmentShader);
+
+    const program = gl.createProgram();
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+
+    gl.linkProgram(program);
+
+    gl.detachShader(program, vertexShader);
+    gl.detachShader(program, fragmentShader);
+
+    gl.validateProgram(program);
+
+    let squareVerts = [
+    //  X,    Y
+        -0.5, -0.5,
+        0.5, -0.5,
+        0.5,  0.5,
+        -0.5,  0.5,            
+
+    ]
+
+    const squareIndices =
+	[
+        0, 1, 2,
+        0, 2, 3
+    ]
+    let colors = [
+    //  r    g    b
+        1.0, 0.0, 0.0,
+        1.0, 0.0, 0.0,
+        1.0, 0.0, 0.0,
+        1.0, 0.0, 0.0
+    ]
+
+    const squareVertBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(squareVerts), gl.STATIC_DRAW);
+
+    const squareIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, squareIndexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(squareIndices), gl.STATIC_DRAW);
+
+    const posAttrLoc = gl.getAttribLocation(program, 'vertPosition');
+    gl.vertexAttribPointer(
+        posAttrLoc,
+        2,
+        gl.FLOAT,
+        gl.FALSE,
+        2 * Float32Array.BYTES_PER_ELEMENT,
+        0
+    );
+
+    gl.enableVertexAttribArray(posAttrLoc);
+
+     const squareColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareColorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    const colorAttrLoc = gl.getAttribLocation(program, 'vertColor');
+    gl.vertexAttribPointer(
+        colorAttrLoc,
+        3,
+        gl.FLOAT,
+        gl.FALSE,
+        3 * Float32Array.BYTES_PER_ELEMENT,
+        0
+    );
+
+    gl.enableVertexAttribArray(colorAttrLoc);
+    // render time
+
+    gl.useProgram(program);
+    gl.drawElements(gl.TRIANGLES, squareIndices.length, gl.UNSIGNED_SHORT, 0)
+
+}
+
+function checkGl(gl) {
+    if (!gl) {console.log('WebGL not supported, use another browser');}
+}
+
+function checkShaderCompile(gl, shader) {
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.error('shader not compiled', gl.getShaderInfoLog(shader));
+    }
+}
+
+function checkLink(gl, program) {
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error('ERROR linking program!', gl.getProgramInfoLog(program));
+    }
+}
